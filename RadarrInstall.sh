384e2315f5df5355fa4a4d28333f088f15bb03d8
Finalfx <finalfx182@gmail.com>
2017-03-29 01:40:52 -0600

    Create RadarrInstall.sh

---
+++ RadarrInstall.sh
@@ -0,0 +1,29 @@
+
+
+  docker create \
+    --name=radarr \
+    -v /local/media/radarr:/config \
+    -v /MediaStore/complete:/downloads \
+    -v /MediaStore:/local/media \
+    -e PGID=1010 -e PUID=1010  \
+    -e TZ="America/Edmonton" \
+    -p 7878:7878 \
+linuxserver/radarr
+
+echo "[Unit]
+Description=radarr container
+Requires=docker.service
+After=docker.service
+
+[Service]
+Restart=always
+ExecStart=/usr/bin/docker start -a radarr
+ExecStop=/usr/bin/docker stop -t 2 radarr
+
+[Install]
+WantedBy=default.target" >/etc/systemd/system/docker-radarr.service
+
+# Enable containers at system startup 
+systemctl daemon-reload
+systemctl enable docker-radarr.service
+systemctl start docker-radarr.service
