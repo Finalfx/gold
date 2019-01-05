4c83cbc53710121a5e04843b7b1feb4053d72000
Finalfx <finalfx182@gmail.com>
2019-01-04 21:20:42 -0700

    Create netdata.sh

---
+++ netdata.sh
@@ -0,0 +1,24 @@
+docker create \
+    --name netdata \
+    -d --cap-add SYS_PTRACE \
+    -v /proc:/host/proc:ro \
+    -v /sys:/host/sys:ro \
+    -p 19999:19999
+    -e PUID=1010 -e PGID=1010 \
+    titpetric/netdata
+
+echo "[Unit]
+Description=netdata container
+Requires=docker.service
+After=docker.service
+[Service]
+Restart=always
+ExecStart=/usr/bin/docker start -a netdata
+ExecStop=/usr/bin/docker stop -t 2 netdata
+[Install]
+WantedBy=default.target" >/etc/systemd/system/docker-netdata.service
+
+# Enable containers at system startup 
+systemctl daemon-reload
+systemctl enable docker-netdata.service
+systemctl start docker-netdata.service
